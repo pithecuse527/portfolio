294bc98867607007c8f869f8cfadd04e9bc94fbc
d_PITHECUSE_b <gihong96@gmail.com>
2021-05-12 13:33:01 +0900

    use smtp to send email via contact-form

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* SmtpJS.com - v3.0.0 */
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
 //javascript for navigation bar effects on scroll
 window.addEventListener("scroll", function(){
   const header = document.querySelector("header");
@@ -21,22 +24,6 @@ navigationItems.forEach((navigationItem) => {
   });
 });
 
-//javascript for scroll to top button
-const scrollBtn = document.querySelector(".scrollToTop-btn");
-
-window.addEventListener("scroll", function(){
-  scrollBtn.classList.toggle("active", window.scrollY > 500);
-});
-
-//javascript for scroll back to top on click scroll-to-top button
-scrollBtn.addEventListener("click", () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
-
-//javascript for reveal website elements on scroll
-window.addEventListener("scroll", reveal);
-
 function reveal(){
   var reveals = document.querySelectorAll(".reveal");
 
@@ -49,4 +36,32 @@ function reveal(){
       reveals[i].classList.add("active");
     }
   }
+}
+
+// Listen for a submit
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  //   Get input Values
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let messages = document.querySelector(".messages").value;
+  console.log(name, email, messages);
+  document.querySelector(".contact-form2").reset();
+  sendEmail(name, email, messages);
+}
+
+// Send Email Info
+function sendEmail(name, email, messages){
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "gihong96@gmail.com",
+    Password: "gttsvjmrwesuxsgf",
+    To: "gihong96@gmail.com",
+    From: "gihong96@gmail.com",
+    Subject: `${name} sent you a message`,
+    Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${messages}`,
+  }).then((messages) => alert("mail sent successfully"))
 }
